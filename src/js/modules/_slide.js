301eaf62787b87356508493f0420156ccc81ea5b
prettymm <895018629@qq.com>
2017-08-02 18:36:14 +0800

    made nav bar on mobile

--- src/js/modules/_slide.js
+++ src/js/modules/_slide.js
@@ -12,7 +12,15 @@ class Slide extends MLP.apps.MLPModule {
       languageDiv: $('#language-div'),
       langClose: $('.lang-close'),
       dropDown: $('.drop-down'),
-      arrowDown: $('.arrow-down')
+      arrowDown: $('.arrow-down'),
+      sidebarDisplay: $('.sidebar-display'),
+      navbarMobile: $('.navbar-mobile'),
+      sidebarSearch: $('.sidebar-search'),
+      form: $('.form'),
+      searchText: $('.SearchText'),
+      dropDownMobile: $('.drop-down-mobile'),
+      navMenuMobile: $('.nav-menu-mobile'),
+      catalent: $('.catalent')
     };
     super.init();
     this.events();
@@ -22,6 +30,7 @@ class Slide extends MLP.apps.MLPModule {
     this.languageEvents();
     this.dropDownEvents();
     this.bodyEvents();
+    this.navbarEvents();
   }
 
   languageEvents() {
@@ -45,6 +54,30 @@ class Slide extends MLP.apps.MLPModule {
       var arrowDown = target.closest('.arrow-down');
       if(!dropDownEle.length && !arrowDown.length){
         dropDown.hide(500);
+      }
+    });
+  }
+
+  navbarEvents() {
+    var _this = this;
+    this.el.sidebarDisplay.on("click", function(){
+      _this.el.catalent.toggleClass("active");
+      _this.el.navbarMobile.toggleClass("active-l-80");
+    });
+
+    this.el.sidebarSearch.on("click", function(){
+      $(this).toggleClass("sidebar-search-changebg");
+      _this.el.form.toggleClass("active-d-b");
+      _this.el.searchText.focus();
+    });
+
+    $('.nav-menu-mobile a').each(function(){
+      if($(this).next(_this.el.dropDownMobile).length){
+        $(this).on('click', function(e){
+          e.stopPropagation();
+          e.preventDefault();
+          $(this).next(_this.el.dropDownMobile).toggle(300);
+        });
       }
     });
   }
